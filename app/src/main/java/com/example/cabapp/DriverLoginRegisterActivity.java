12316927538177cb55acf9198225c94df9ff8ac4
amanagrawal <amanagrawal2098@gmail.com>
2020-12-13 15:16:28 +0530

    gmap added

--- app/src/main/java/com/example/cabapp/DriverLoginRegisterActivity.java
+++ app/src/main/java/com/example/cabapp/DriverLoginRegisterActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -85,6 +86,8 @@ public class DriverLoginRegisterActivity extends AppCompatActivity {
                             {
                                 loadingBar.dismiss();
                                 Toast.makeText(DriverLoginRegisterActivity.this, "Customer Reg Successfully", Toast.LENGTH_SHORT).show();
+                                Intent intent=new Intent(DriverLoginRegisterActivity.this,DriverMapActivity.class);
+                                startActivity(intent);
                             }
                             else {
                                 loadingBar.dismiss();
@@ -116,6 +119,8 @@ public class DriverLoginRegisterActivity extends AppCompatActivity {
                             {
                                 loadingBar.dismiss();
                                 Toast.makeText(DriverLoginRegisterActivity.this, "Driver Login Successfully", Toast.LENGTH_SHORT).show();
+                                Intent intent=new Intent(DriverLoginRegisterActivity.this,DriverMapActivity.class);
+                                startActivity(intent);
                             }
                             else {
                                 loadingBar.dismiss();
